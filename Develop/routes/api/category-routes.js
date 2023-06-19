59190709ab369c2aff568d1dace9a7a25d310f99
anju <anjualfino@gmail.com>
2023-06-20 00:59:30 +0800

    adding routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -42,8 +42,8 @@ router.post('/', async (req, res) => {
   try{
     const category_data=await Category.create(req.body);
     res.status(200).json(category_data);
-  }catch{
-
+  }catch (err) {
+    res.status(500).json(err);
   }
 });
 
