fcedbc7da719251b1fc62a9be7d922f34eded68c
anju <anjualfino@gmail.com>
2023-06-19 11:48:55 +0800

    adding the models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+  const categories=await Category.findAll({
+    include : Product
+  })
+  res.json(categories);
+} catch(error){
+  console.error(error);
+  res.status(500).json({ message: 'Internal server error' });
+}
 });
 
 router.get('/:id', (req, res) => {
